2bba25b651557904bea8147acfa92befb1457bf0
Brian151 <Brian151@users.noreply.github.com>
2017-08-18 13:06:52 -0600

    gfx asset classes converted to stamps, and most likely broken temporarily

--- GameLib_v2/assets/gfx/Sprite.js
+++ GameLib_v2/assets/gfx/Sprite.js
@@ -1,115 +1,145 @@
-var Sprite = function(x,y,img,animationData) {
-	this.spriteSheetImage = img;
-	this.animationData = animationData;
-	this.x = x;
-	this.y = y;
-	this.currFrame = 0;
-	this.currLabel = this.animationData.labels[0];
-	this.stopped = false;
-	this.constrained = false;
-	this.timer = 0;
-	this.tMode = this.animationData.timingMode;
-	this.timing = this.animationData.timing[this.tMode];
-	this.frameMin = 0;
-	this.frameMax = this.animationData.length - 1;
-	this.rate = 1000 / this.timing;
-	this.deltaTime = 0;
-	this.timeNow = Date.now();
-	this.timeLast = this.timeNow;
-}
-Sprite.prototype.tick = function() {
-	//this.findCurrentLabel();
-	if(!this.stopped){
-		switch(this.tMode) {
-			case 0:
-				if(!(this.timer % this.timing) && this.timer != 0) {
-					this.currFrame++;
-					if (this.currFrame == (this.frameMax + 1)) this.currFrame = this.frameMin;
-					this.timer = 0;
-					//console.log(this.currFrame);
-				}
-				this.timer++;
-				//console.log(this.timeLast);
+(function(){
+"use strict";
+var Sprite = stampit.deepProps({
+	spriteSheetImage : {},
+	animationData : {},
+	timeNow : {},
+	timeLast : {},
+	target : {}
+}).props({
+	x : x;
+	y : y;
+	currFrame : 0;
+	currLabel : "";
+	stopped : false;
+	constrained : false;
+	timer : 0;
+	tMode : 0;
+	timing : 0;
+	frameMin : 0;
+	frameMax : 0;
+	rate : 0;
+	deltaTime : 0;
+}).methods({
+	attach : function(context) {
+		this.target = context;
+	},
+	create : function() {
+		this.spriteSheetImage = img;
+		this.animationData = animationData;
+		this.x = x;
+		this.y = y;
+		this.currFrame = 0;
+		this.currLabel = this.animationData.labels[0];
+		this.stopped = false;
+		this.constrained = false;
+		this.timer = 0;
+		this.tMode = this.animationData.timingMode;
+		this.timing = this.animationData.timing[this.tMode];
+		this.frameMin = 0;
+		this.frameMax = this.animationData.length - 1;
+		this.rate = 1000 / this.timing;
+		this.deltaTime = 0;
+		this.timeNow = Date.now();
+		this.timeLast = this.timeNow;
+	},
+	tick : function() {
+		//this.findCurrentLabel();
+		if(!this.stopped){
+			switch(this.tMode) {
+				case 0:
+					if(!(this.timer % this.timing) && this.timer != 0) {
+						this.currFrame++;
+						if (this.currFrame == (this.frameMax + 1)) this.currFrame = this.frameMin;
+						this.timer = 0;
+						//console.log(this.currFrame);
+					}
+					this.timer++;
+					//console.log(this.timeLast);
+					break;
+				case 1: 
+					this.timeNow = Date.now();
+					this.deltaTime += (this.timeNow - this.timeLast) / this.rate;
+					this.timeLast = this.timeNow;
+					if (this.deltaTime >= 1) {
+						this.deltaTime = 0;
+						this.currFrame++;
+						if (this.currFrame == (this.frameMax + 1)) this.currFrame = this.frameMin;
+					//console.log(this.deltaTime);
+					}
+					break;
+			}
+		}
+	},
+	draw : function(){
+		//console.log("draw!");
+		this.tick();
+		var frame = this.animationData.frames[this.currFrame];
+		GameObjs.renderer.drawClippedImage(this.spriteSheetImage,this.x,this.y,this.animationData.frameWidth,this.animationData.frameHeight,frame.x,frame.y);
+	},
+	gotoAndStop : function(frame){
+		this.constrained = false;
+		this.stopped = true;
+		var foundLabel = false;
+		for (var i=0; i < this.animationData.labels.length; i++) {
+			var curr = this.animationData.labels[i];
+			if (curr.name == frame) {
+				foundLabel = true;
+				this.currLabel = curr;
 				break;
-			case 1: 
-				this.timeNow = Date.now();
-				this.deltaTime += (this.timeNow - this.timeLast) / this.rate;
-				this.timeLast = this.timeNow;
-				if (this.deltaTime >= 1) {
-					this.deltaTime = 0;
-					this.currFrame++;
-					if (this.currFrame == (this.frameMax + 1)) this.currFrame = this.frameMin;
-				//console.log(this.deltaTime);
-				}
+			}
+		}
+		if (foundLabel) this.currFrame = this.currLabel.i;
+		if (!foundLabel) this.currFrame = frame;
+	},
+	gotoAndPlay : function(frame){
+		this.constrained = false;
+		this.stopped = false;
+		var foundLabel = false;
+		for (var i=0; i < this.animationData.labels.length; i++) {
+			var curr = this.animationData.labels[i];
+			if (curr.name == frame) {
+				foundLabel = true;
+				this.currLabel = curr;
 				break;
+			}
 		}
-	}
-}
-Sprite.prototype.draw = function(){
-	//console.log("draw!");
-	this.tick();
-	var frame = this.animationData.frames[this.currFrame];
-	GameObjs.renderer.drawClippedImage(this.spriteSheetImage,this.x,this.y,this.animationData.frameWidth,this.animationData.frameHeight,frame.x,frame.y);
-}
-Sprite.prototype.gotoAndStop = function(frame) {
-	this.constrained = false;
-	this.stopped = true;
-	var foundLabel = false;
-	for (var i=0; i < this.animationData.labels.length; i++) {
-		var curr = this.animationData.labels[i];
-		if (curr.name == frame) {
-			foundLabel = true;
-			this.currLabel = curr;
-			break;
+		if (foundLabel) this.currFrame = this.currLabel.i;
+		if (!foundLabel) this.currFrame = frame;
+		this.timer = 0;
+	},
+	/*
+		specifically since I don't want work with nested animations just yet,
+		and also because the Sprite object uses spritesheets, raising
+		some questions how this implementation should work exactly
+	*/
+	gotoAndPlayWithin : function(frame){
+		this.stopped = false;
+		var foundLabel = false;
+		for (var i=0; i < this.animationData.labels.length; i++) {
+			var curr = this.animationData.labels[i];
+			if (curr.name == frame) {
+				foundLabel = true;
+				this.currLabel = curr;
+				this.frameMin = curr.i;
+				this.frameMax = curr.e;
+				this.timing = this.animationData.timings[frame][this.tMode];
+				this.rate = 1000 / this.timing;
+				//console.log(this.timing);
+				break;
+			}
 		}
-	}
-	if (foundLabel) this.currFrame = this.currLabel.i;
-	if (!foundLabel) this.currFrame = frame;
-}
-Sprite.prototype.gotoAndPlay = function(frame) {
-	this.constrained = false;
-	this.stopped = false;
-	var foundLabel = false;
-	for (var i=0; i < this.animationData.labels.length; i++) {
-		var curr = this.animationData.labels[i];
-		if (curr.name == frame) {
-			foundLabel = true;
-			this.currLabel = curr;
-			break;
+		if (foundLabel) this.currFrame = this.currLabel.i;
+		if (!foundLabel) {
+			console.log("failed to find label: " + frame);
 		}
+		this.timer = 0;
+	},
+	play : function(){
+		this.stopped = false;
+	},
+	stop : function() {
+		this.stopped = true;
 	}
-	if (foundLabel) this.currFrame = this.currLabel.i;
-	if (!foundLabel) this.currFrame = frame;
-	this.timer = 0;
-}
-/*specifically since I don't want work with nested animations just yet,
-and also because the Sprite object uses spritesheets, raising
-some questions how this implementation should work exactly*/
-Sprite.prototype.gotoAndPlayWithin = function(frame) {
-	this.stopped = false;
-	var foundLabel = false;
-	for (var i=0; i < this.animationData.labels.length; i++) {
-		var curr = this.animationData.labels[i];
-		if (curr.name == frame) {
-			foundLabel = true;
-			this.currLabel = curr;
-			this.frameMin = curr.i;
-			this.frameMax = curr.e;
-			this.timing = this.animationData.timings[frame][this.tMode];
-			this.rate = 1000 / this.timing;
-			//console.log(this.timing);
-			break;
-		}
-	}
-	if (foundLabel) this.currFrame = this.currLabel.i;
-	if (!foundLabel) {
-		console.log("failed to find label: " + frame);
-	}
-	this.timer = 0;
-}
-Sprite.prototype.play = function() {
-	this.stopped = false;
-}
-Sprite.prototype.stop = function() {
-	this.stopped = true;
-}
+});
+QualityCatGameLibrary.attachModule(Sprite,"Sprite");})();
--- GameLib_v2/assets/gfx/SpriteSheet.js
+++ GameLib_v2/assets/gfx/SpriteSheet.js
@@ -1,55 +1,71 @@
-var SpriteData = function(configData) {
-	//copy template attributes to the new SpriteData object
-	this.width = configData.width;
-	this.height = configData.height;
-	this.x = configData.x;
-	this.y = configData.y;
-	this.frameWidth = configData.sW;
-	this.frameHeight = configData.sH;
-	this.frames = [];
-	this.labels = configData.labels;
-	this.length = configData.length;
-	this.timings = configData.timings;
-	this.timing = configData.timing;
-	this.timingMode = configData.timingMode;
-	var excludedFrames = configData.cutFrames;
-	
-	var i3 = 0; //id/index of the current frame
-	//loop the various 'rows' of the sheet
-	for (var i = 0; i < this.height; i += this.frameHeight) {
-		//loop the various 'columns'/'cells' of the sheet
-		for (var i2 = 0; i2 < this.width; i2 += this.frameWidth) {
-			if (i2 + this.frameHeight <= this.width) {
-				var copyFrame = true; //by default, we assume the current frame should be copied
-				//loop the excluded frames array
-				for(var i4=0; i4 < excludedFrames.length; i4++) {
-					var curr = excludedFrames[i4];
-					//the currrent frame is the same as an excluded frame, do not copy it
-					if (curr == i3) {
-						copyFrame = false;
-						//console.log("do not copy frame! (" + curr + ")");
-						excludedFrames.splice(i4,1); //no longer need this exclusion index
-						break;
+(function(){
+"use strict";
+var SpriteSheet = stampit.props({
+	x : 0,
+	y : 0,
+	frameWidth : 0,
+	frameHeight : 0,
+	length : 0,
+	timing : 0,
+	timingMode : 1
+}).deepProps({
+	frames : [],
+	labels : [],
+	timings : []
+}).methods({
+	create : function(configData) {
+		//copy template attributes to the new SpriteData object
+		this.width = configData.width;
+		this.height = configData.height;
+		this.x = configData.x;
+		this.y = configData.y;
+		this.frameWidth = configData.sW;
+		this.frameHeight = configData.sH;
+		this.labels = configData.labels;
+		this.length = configData.length;
+		this.timings = configData.timings;
+		this.timing = configData.timing;
+		this.timingMode = configData.timingMode;
+		var excludedFrames = configData.cutFrames;
+		
+		var i3 = 0; //id/index of the current frame
+		//loop the various 'rows' of the sheet
+		for (var i = 0; i < this.height; i += this.frameHeight) {
+			//loop the various 'columns'/'cells' of the sheet
+			for (var i2 = 0; i2 < this.width; i2 += this.frameWidth) {
+				if (i2 + this.frameHeight <= this.width) {
+					var copyFrame = true; //by default, we assume the current frame should be copied
+					//loop the excluded frames array
+					for(var i4=0; i4 < excludedFrames.length; i4++) {
+						var curr = excludedFrames[i4];
+						//the currrent frame is the same as an excluded frame, do not copy it
+						if (curr == i3) {
+							copyFrame = false;
+							//console.log("do not copy frame! (" + curr + ")");
+							excludedFrames.splice(i4,1); //no longer need this exclusion index
+							break;
+						}
 					}
+					//we are supposed to copy this frame, so lets copy it
+					if (copyFrame) 
+						this.frames.push({
+							"x":this.x + i2,
+							"y":this.y + i,
+							"w":this.frameWidth,
+							"h":this.frameHeight
+							});
+					i3++; //increment the frame counter
+				} else {
+					break;
 				}
-				//we are supposed to copy this frame, so lets copy it
-				if (copyFrame) 
-					this.frames.push({
-						"x":this.x + i2,
-						"y":this.y + i,
-						"w":this.frameWidth,
-						"h":this.frameHeight
-						});
-				i3++; //increment the frame counter
+			}
+			if (i + this.frameHeight <= this.height) {
+				//nothing needs happen
 			} else {
 				break;
 			}
 		}
-		if (i + this.frameHeight <= this.height) {
-			//nothing needs happen
-		} else {
-			break;
-		}
+		//console.log(JSON.stringify(this.frames));
 	}
-	//console.log(JSON.stringify(this.frames));
-}
+});
+QualityCatGameLibrary.attachModule(SpriteSheet,"SpriteSheet");})();
